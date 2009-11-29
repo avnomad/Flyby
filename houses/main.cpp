2bf191c48321a16ae86c16be1f1bca50b5a2b02b
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2009-11-29 17:45:39 +0000

    2nd version. can move and look around (mouse and keyboard). 2 toruses and 4 spheres. lighting enabled.
    --HG--
    extra : convert_revision : svn%3A7e7d06bd-a938-0f47-88fa-0f618b0aaabd/trunk%4014

--- houses/main.cpp
+++ houses/main.cpp
@@ -1,14 +1,32 @@
 #include <gl/glew.h>
 #include <gl/glut.h>
 #include <cstdlib>
+#include <cmath>
 
+#define PI 3.1415926535897932385
+
+// globals
+double px = 0;
+double py = 65;
+double pz = 0;
+double fi = 0;
+double theta = -90;
+
+int oldx = 0;
+int oldy = 0;
+
+const double farClip = 300;
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glLoadIdentity();
-	gluLookAt(40,60,0,0,0,0,0,0,1);
+	glRotated(90,0,1,0);
+	glRotated(-90,1,0,0);
+	glRotated(fi,0,1,0);
+	glRotated(-theta,0,0,1);
+	glTranslated(-px,-py,-pz);
 
 
 	//GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
@@ -18,11 +36,23 @@ void display()
 	glutSolidSphere(10,100,100);
 	glPushMatrix();
 		glTranslated(15,0,0);
+		glPushMatrix();
+			glTranslated(0,0,15);
+			glutSolidSphere(7,100,100);
+		glPopMatrix();
+		glPushMatrix();
+			glTranslated(0,15,0);
+			glutSolidSphere(7,100,100);
+		glPopMatrix();
 		glRotated(90,0,1,0);
 		glutSolidTorus(5,15,100,100);
 	glPopMatrix();
 	glPushMatrix();
 		glTranslated(-15,0,0);
+		glPushMatrix();
+			glTranslated(0,0,15);
+			glutSolidSphere(7,100,100);
+		glPopMatrix();
 		glRotated(90,0,1,0);
 		glutSolidTorus(5,15,100,100);
 	glPopMatrix();
@@ -31,13 +61,66 @@ void display()
 } // end function display
 
 
+void mouse(int key, int state, int x, int y)
+{
+	if(key == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		oldx = x;
+		oldy = y;
+	} // end if
+} // end function mouse
+
+
+void mouseMove(int x, int y)
+{
+	theta -= x-oldx;
+	if(theta > 180)
+		theta -= 360;
+	if(theta < -180)
+		theta += 360;
+	fi -= y-oldy;
+	if(fi>90)
+		fi = 90;
+	if(fi< -90)
+		fi = -90;
+	oldx = x;
+	oldy = y;
+	glutPostRedisplay();
+} // end function mouseMove
+
+
 void keyboard(unsigned char key, int x, int y)
 {
 	switch(key)
 	{
+	case 'a':	// left
+		px += cos(PI*theta/180.0+PI/2);
+		py += sin(PI*theta/180.0+PI/2);
+		break;
+	case 'd':	// right
+		px -= cos(PI*theta/180.0+PI/2);
+		py -= sin(PI*theta/180.0+PI/2);
+		break;
+	case 'w':	// forward
+		px += cos(PI*fi/180.0)*cos(PI*theta/180.0);
+		py += cos(PI*fi/180.0)*sin(PI*theta/180.0);
+		pz += sin(PI*fi/180.0);
+		break;
+	case 's':	// backwards
+		px -= cos(PI*fi/180.0)*cos(PI*theta/180.0);
+		py -= cos(PI*fi/180.0)*sin(PI*theta/180.0);
+		pz -= sin(PI*fi/180.0);
+		break;
+	case ' ':	// up
+		pz += 1;
+		break;
+	case 'c':	// down
+		pz -= 1;
+		break;
 	case 27:	// escape key
 		exit(0);
 	} // end switch
+	glutPostRedisplay();
 } // end function keyboard
 
 
@@ -46,9 +129,15 @@ void reshape (int w, int h)
 	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60,w/(GLdouble)h,5,100);
+	gluPerspective(60,w/(GLdouble)h,1,farClip);
 	glMatrixMode(GL_MODELVIEW);
 } // end function reshape
+
+
+void always()
+{
+	glutPostRedisplay();
+} // end function always
 
 
 int main(int argc, char **argv)
@@ -59,12 +148,13 @@ int main(int argc, char **argv)
 	glutInitWindowSize(320,320);
 	glutInitWindowPosition(800,160);
 	glutCreateWindow("Creating Geometry");
+	glutSetKeyRepeat(GLUT_KEY_REPEAT_ON);
 
 	// glew initialization
 	glewInit();
 
 	// OpenGL initialization
-	glClearDepth(100);
+	glClearDepth(farClip);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
@@ -75,8 +165,11 @@ int main(int argc, char **argv)
 
 	// event handling initialization
 	glutDisplayFunc(display);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMove);
 	glutKeyboardFunc(keyboard);
 	glutReshapeFunc(reshape);
+	glutIdleFunc(always);
 	glutMainLoop();
 	return 0;
 } // end function main
